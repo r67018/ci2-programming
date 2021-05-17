228c807d2d0218c7bb73c3cf07784d8395199fb7
swk67018 <swear1208a13@gmail.com>
2021-05-17 09:15:36 +0900

    Fix chapter3/setsumon10.c

--- chapter3/setsumon10.c
+++ chapter3/setsumon10.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
     int a, b;
     scanf("%d%d", &a, &b);
 
-    double a == 0;
+    if ((a - b <= 10 && a - b >= 0) || (b - a <= 10 && b - a >= 0)) {
+        printf("<=10");
+    } else {
+        printf(">10");
+    }
+
+    if (a < b) {
+        printf(" A<B");
+    }
+    printf("\n");
 
     return 0;
 }
