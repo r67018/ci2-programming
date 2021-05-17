7c87d76adfae78974c0869caec384b269c89366c
swk67018 <swear1208a13@gmail.com>
2021-05-17 09:33:14 +0900

    Fix chapter4/setsumon1.c

--- chapter4/setsumon1.c
+++ chapter4/setsumon1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    int n;
-    scanf("%d", &n);
 
-    if (n > 0) {
-        puts("positive");
-    } else if (n < 0) {
-        puts("negative");
-    } else {
-        puts("zero");
-    }
+    int n;
+    do {
+        scanf("%d", &n);
+        if (n > 0) {
+            puts("positive");
+        } else if (n < 0) {
+            puts("negative");
+        } else {
+            puts("zero");
+        }
+        scanf("%d", &n);
+    } while (n == 0);
 
     return 0;
 }
