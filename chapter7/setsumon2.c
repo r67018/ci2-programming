d79064557d0fa7d8f6fa12c691b29e189f6d5d79
swk67018 <swear1208a13@gmail.com>
2021-07-21 10:07:41 +0900

    Update chapter7/setsumon2.c setsumon8.c

--- chapter7/setsumon2.c
+++ chapter7/setsumon2.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+void print_bits(unsigned x) {
+    int i;
+    for (i = 31; i >= 0; i--) {
+        if (x & (1 << i)) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
 int main(void) {
     unsigned x;
     scanf("%u", &x);
+    print_bits(x);
 
-    printf("左シフト: %u\n", (x<<1));
-    printf("右シフト: %u\n", (x>>1));
+    printf("%u\n", (x<<1));
+    print_bits(x<<1);
+
+    printf("%u\n", (x>>1));
+    print_bits(x>>1);
 
     return 0;
 }
