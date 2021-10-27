a842f10f3464b2f46c49c347a2990b63947524e6
swk67018 <swear1208a13@gmail.com>
2021-10-27 15:55:58 +0900

    Fix code

--- chapter9/setsumon11.c
+++ chapter9/setsumon11.c
@@ -4,6 +4,16 @@
 #define NUM     128
 #define LEN_MAX 128
 
+int input_strary(char s[NUM][LEN_MAX]) {
+    int count = 0;
+    while (1) {
+        scanf("%s", s[count]);
+        if (strcmp(s[count], "$$$$$") == 0) break;
+        count++;
+    }
+    return count;
+}
+
 void put_strary(const char s[NUM][LEN_MAX], int n) {
     int i;
     for (i = 0; i < n; i++) {
@@ -15,13 +25,7 @@ int main(void) {
     int i;
     char cs[NUM][LEN_MAX];
 
-    int count = 0;
-    while (1) {
-        scanf("%s", cs[count]);
-        if (strcmp(cs[count], "$$$$$") == 0) break;
-        count++;
-    }
-
+    int count = input_strary(cs);
     put_strary(cs, count);
 
     return 0;
